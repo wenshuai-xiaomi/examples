6162ceab42e74955dd09cff74ea0e4e223fc53e5
wenshuai <wenshuai@xiaomi.com>
2021-09-29 04:37:48 -0230

    chage file name to hash to avoid repeated.
    Signed-off-by: wenshuai <wenshuai@xiaomi.com>

--- lite/examples/speech_commands/android/split_wav_1s.py
+++ lite/examples/speech_commands/android/split_wav_1s.py
@@ -2,10 +2,10 @@ import wave
 import os
 from pydub import AudioSegment
 from pydub.silence import split_on_silence
-
+import hashlib
 
 # 初始化
-audiopath = "/home/ws/datasets/backup/pcms/wavsfromBC/back_ground/20210928-103116932.wav"
+audiopath = "/home/ws/datasets/backup/pcms/wavsfromBC/back_ground/20210929-101043358.wav"
 audiotype = 'wav' #如果wav、mp4其他格式参看pydub.AudioSegment的API
 
 cut_time=1  #音频切割间隔1s
@@ -21,24 +21,28 @@ file_num=int(num)
 print('wav file num = ',num, 'loss 1')
 
 filepath = os.path.split(audiopath)[0]  #获取当前处理音频的路径
-cuts_path = filepath+'/cuts/'
+cuts_path = filepath + '/cuts/'
 print(cuts_path)
 
 if not os.path.isdir(cuts_path):
         os.makedirs(cuts_path)
 
+md5 = hashlib.md5()
+
 for i in range(file_num):     #加1是因为分割数可能是小数
-    begin=0 + cut_time*i*1000
-    end=begin+cut_time*1000
+    begin = 0 + cut_time * i *1000
+    end = begin + cut_time * 1000
+
     if end/1000 > file_num:
-        end=sound.duration_seconds*1000
-    print('begin=',begin,'end=',end)
-    cut_file_path=cuts_path+'cutxxx'+'.wav'
-    if i < 10:
-        cut_file_path=cut_file_path.replace('xxx','0'+str(i))
-    else:
-        cut_file_path=cut_file_path.replace('xxx',str(i))
+        end = sound.duration_seconds*1000
+
+    print('begin= ', begin, 'end= ', end)
+
+    md5.update((cuts_path + str(i)).encode('utf-8'))
+    cut_file_path = cuts_path + md5.hexdigest() + '.wav'
     
     cut_wav = sound[begin:end]   #以毫秒为单位截取[begin, end]区间的音频
+
     cut_wav.export(cut_file_path, format='wav')   #存储新的wav文件
 print('保存完毕')
+
